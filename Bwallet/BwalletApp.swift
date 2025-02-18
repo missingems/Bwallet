06ac928c03ac389ab16c2dc01d999d71da8a4f2b
Jun <51901925+missingems@users.noreply.github.com>
2025-02-18 12:26:30 +0800

    Initial Commit

---
+++ Bwallet/BwalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BwalletApp.swift
+//  Bwallet
+//
+//  Created by Jun on 18/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BwalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BwalletUITests/BwalletUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BwalletUITestsLaunchTests.swift
+//  BwalletUITests
+//
+//  Created by Jun on 18/2/25.
+//
+
+import XCTest
+
+final class BwalletUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
